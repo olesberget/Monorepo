bc0fa2647f8b9d58edca1550e934f5eb9ffbd4d4
olesberget <ole.s.berget@gmail.com>
2024-12-10 14:31:21 +0100

    fix

--- apps/mafia/WebApplication1/Controllers/AccountController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using WebApplication1.Models;
-
--- apps/mafia/WebApplication1/clientapp/src/Components/GameInfo.js
+++
@@ -1,9 +0,0 @@
-﻿import React from 'react';
-function GameInfo({ gameId }) {
-    return (
-        <div className="game-info">
-            <p className="TitleGameInfo">Game Id:</p> <p className="TitleGameInfo">{gameId}</p>
-        </div>
-    );
-}
-export default GameInfo;
